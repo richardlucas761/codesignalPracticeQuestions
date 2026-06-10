150d5aba9f46eeeda008c84c5b8ef196270b4ca7
richard <richardlucas761@protonmail.com>
2026-06-10 11:32:25 +0100

    Add solution for ProcessQueries which implements the unit tests. Add ProcessQueries to README with notes.

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ Implementations of the practice questions on codesignal.
 
 For a given integer value returns the sum of the digits so an input of 29 gives an output of 11 (2 + 9 = 11).
 
+## ProcessQueries
+
+Process a list of operations to either ADD or check whether a specified integer "EXISTS".
+
+This method uses a Jagged Array: https://stackoverflow.com/questions/2576759/what-is-a-jagged-array 
+
+SonarQube suggests this should be fixed so we probably wouldn't use this in real code, see csharpsquid:S2368 (Public methods should not have multidimensional array parameters).
+
 ## Notes
 
 The solutions have been implemented in .NET 10 as the version used for the Code Signal tests is no longer active (.NET v9.0.203).
--- codesignalPracticeQuestionsCSharp/codesignalPracticeQuestionsCSharp/Program.cs
+++ codesignalPracticeQuestionsCSharp/codesignalPracticeQuestionsCSharp/Program.cs
@@ -20,8 +20,29 @@
 
         public static string[] ProcessQueries(string[][] queries)
         {
-            // TODO implement
-            return [];
+            var integers = new List<int>();
+            var output = new List<string>();
+
+            foreach (var query in queries)
+            {
+                var operation = query[0];
+                var parameter = query[1];
+
+                var parameterAsInt = int.Parse(parameter);
+
+                if (operation == "ADD")
+                {
+                    integers.Add(parameterAsInt);
+                    output.Add(string.Empty);
+                }
+                else if (operation == "EXISTS")
+                {
+                    // Adds "true" or "false"
+                    output.Add(integers.Contains(parameterAsInt).ToString().ToLowerInvariant());
+                }
+            }
+
+            return [.. output];
         }
 
         public static void Main()
