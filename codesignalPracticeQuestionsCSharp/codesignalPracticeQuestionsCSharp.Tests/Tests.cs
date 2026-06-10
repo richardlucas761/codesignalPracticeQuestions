f33ef6b0d39122de6a369594935bf5c81b61b0de
richard <richardlucas761@protonmail.com>
2026-06-10 10:30:38 +0100

    Refactoring. Add implementation of first practice question with tests.

---
+++ codesignalPracticeQuestionsCSharp/codesignalPracticeQuestionsCSharp.Tests/Tests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+
+namespace codesignalPracticeQuestionsCSharp.Tests
+{
+    [TestClass]
+    public sealed class Tests
+    {
+        [TestMethod]
+        [DataRow(29, 11)]
+        [DataRow(48, 12)]
+        [DataRow(10, 1)]
+        [DataRow(25, 7)]
+        [DataRow(52, 7)]
+        [DataRow(99, 18)]
+        [DataRow(44, 8)]
+        [DataRow(50, 5)]
+        [DataRow(39, 12)]
+        [DataRow(26, 8)]
+        public void AddTwoDigitsTest(int value, int expected)
+        {
+            // Arrange
+
+            // Act
+            var result = Program.AddTwoDigits(value);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
--- codesignalPracticeQuestionsCSharp/codesignalPracticeQuestionsCSharp/Program.cs
+++ codesignalPracticeQuestionsCSharp/codesignalPracticeQuestionsCSharp/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace codesignalPracticeQuestionsCSharp
+{
+    public static class Program
+    {
+        public static int AddTwoDigits(int n)
+        {
+            int total = 0;
+
+            foreach (var digit in n.ToString())
+            {
+                // See https://learn.microsoft.com/en-us/dotnet/api/system.char
+                var result = char.GetNumericValue(digit);
+
+                // GetNumericValue returns a double so we must cast to int
+                total += (int)result;
+            }
+
+            return total;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
